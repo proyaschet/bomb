320f0985fe095e18d94b522ab83e93bcf2cffe32
Amarnath <amarnath@Amarnaths-MacBook-Air.local>
2016-06-30 21:09:23 +0530

    practice added

--- practice/ViewController.swift
+++ practice/ViewController.swift
@@ -10,6 +10,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var blue: UIImageView!
+    
+    @IBOutlet weak var red: UIImageView!
+    @IBOutlet weak var hideRed: UIButton!
+    @IBOutlet weak var hideBLue: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +25,13 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func hideRedbomb(sender: AnyObject) {
+        red.hidden=true
+    }
+    
+    @IBAction func hideBluebomb(sender: AnyObject) {
+        blue.hidden=true
+    }
 
 }
 
